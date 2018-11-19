833d9fd6553ca09137a0ba224bcff35d19c30265
unknown <you@example.com>
2018-11-20 01:17:04 +0800

    store

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FootGuide v-show="$route.meta.showFoot"/>
+  </div>
+</template>
+
+<script>
+  import FootGuide from './components/FootGuide/FootGuide.vue'
+
+
+  export default {
+
+    mounted () {
+      this.$store.dispatch('getAddress')
+    },
+
+    components: {
+      FootGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
